9c60b54574f279b13771deec3a41075e91462949
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-06 15:11:28 +0200

    Add extension class for drugs

---
+++ DotNET/BusinessLayer/Data/DrugExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.BusinessLayer.Data
+{
+    public partial class Drug
+    {
+        public string dummyMethod()
+        {
+            return "Hello world";
+        }
+    }
+}
