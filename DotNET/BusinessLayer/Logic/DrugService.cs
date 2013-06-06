11a43a3e7773172dabcaad04bbe65be3a98d4265
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-06 13:31:46 +0200

    Add simple drug service

---
+++ DotNET/BusinessLayer/Logic/DrugService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pharmacy.BusinessLayer.Data;
+
+namespace Pharmacy.BusinessLayer.Logic
+{
+    public static class DrugService
+    {
+        public static ICollection<Drug> GetAllDrugs()
+        {
+            using (PharmacyContainer db = new PharmacyContainer())
+                return db.DrugSet.ToList();
+        }
+    }
+}
