be1a42ae76d96423ad39a88cb33cc400a8bca446
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-11 18:00:15 +0200

    Adjust class name for drug list

--- DotNET/WebLayer/Drug/List.aspx.cs
+++ DotNET/WebLayer/Drug/List.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebLayer
+namespace WebLayer.Drug
 {
-    public partial class Main : System.Web.UI.Page
+    public partial class List : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
