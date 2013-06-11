e4ee4868d5709224569d3406f4dd43d9e38f2dff
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-11 18:00:00 +0200

    Add error page (not yet triggered)

---
+++ DotNET/WebLayer/Error.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="Error.aspx.cs" Inherits="WebLayer.Errors" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentHolder" runat="server">
+    <form id="form1" runat="server">
+    Your request could not be processed.<br />
+    <asp:Label ID="ErrorLabel" runat="server" CssClass="error"></asp:Label>
+    </form>
+</asp:Content>
---
+++ DotNET/WebLayer/Error.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebLayer
+{
+    public partial class Errors : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Exception lastError = Session["LastError"] as Exception;
+            Session.Remove("LastError");
+            if (lastError != null)
+            {
+                if (lastError is System.Reflection.TargetInvocationException)
+                    lastError = lastError.InnerException;
+                ErrorLabel.Text = lastError.Message;
+            }
+        }
+    }
+}
