962863aa14564bc9b823eb6ab1b1d960ec641253
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-13 16:54:16 +0200

    Add proceeding of orders

--- DotNET/BusinessLayer/Logic/OrderService.cs
+++ DotNET/BusinessLayer/Logic/OrderService.cs
@@ -19,12 +19,51 @@ namespace Pharmacy.BusinessLayer.Logic
         {
             using (PharmacyContainer db = new PharmacyContainer())
             {
-                ReplenishmentOrder result = (from o in db.ReplenishmentOrderSet where o.Id == id select o).FirstOrDefault();
+                return GetOrder(id, db);
+            }
+        }
 
-                if (result == default(ReplenishmentOrder))
-                    throw new ArgumentException(String.Format("Order with ID {0} not found", id.ToString()));
+        internal static ReplenishmentOrder GetOrder(Int32 id, PharmacyContainer db)
+        {
+            ReplenishmentOrder result = (from o in db.ReplenishmentOrderSet where o.Id == id select o).FirstOrDefault();
 
-                return result;
+            if (result == default(ReplenishmentOrder))
+                throw new ArgumentException(String.Format("Order with ID {0} not found", id.ToString()));
+
+            return result;
+        }
+
+        public static void UpdateExpectedDeliveryDate(Int32 id, DateTime expectedDelivery)
+        {
+            using (PharmacyContainer db = new PharmacyContainer())
+            {
+                ReplenishmentOrder order = GetOrder(id, db);
+                order.ExpectedDelivery = expectedDelivery;
+                db.SaveChanges();
+            }
+        }
+
+        public static void UpdateActualDeliveryDate(Int32 id, DateTime actualDelivery)
+        {
+            using (PharmacyContainer db = new PharmacyContainer())
+            {
+                ReplenishmentOrder order = GetOrder(id, db);
+                order.ActualDelivery = actualDelivery;
+                db.SaveChanges();
+            }
+        }
+
+        public static void ProceedToNextState(Int32 id)
+        {
+            using (PharmacyContainer db = new PharmacyContainer())
+            {
+                ReplenishmentOrder order = GetOrder(id, db);
+                order.State = order.State.Next();
+                if (order.State == OrderState.Finished)
+                {
+                    // replenish all positions
+                }
+                db.SaveChanges();
             }
         }
     }
--- DotNET/WebLayer/ReplenishmentOrder/Details.aspx.cs
+++ DotNET/WebLayer/ReplenishmentOrder/Details.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Globalization;
 using Pharmacy.BusinessLayer.Data;
 using Pharmacy.BusinessLayer.Logic;
 
@@ -16,17 +17,43 @@ namespace WebLayer.ReplenishmentOrder
 
         }
 
+        private void Page_Error(object sender, EventArgs e)
+        {
+            Session["LastError"] = Server.GetLastError();
+            Server.ClearError();
+            Server.Transfer("~/Error.aspx");
+        }
+
         protected void NextStateButton_Command(object sender, CommandEventArgs e)
         {
             if (!Page.IsValid)
                 return;
-            // TODO proceed to next state
+
+            Pharmacy.BusinessLayer.Data.ReplenishmentOrder order = OrderService.GetOrder(Int32.Parse(Request.Params["id"]));
+            if (order.State == OrderState.Posting)
+            {
+                string expectedDelivery = ((TextBox)OrderDetailsView.FindControl("ExpectedDeliveryBox")).Text;
+                OrderService.UpdateExpectedDeliveryDate(order.Id, parseDateTime(expectedDelivery));
+            }
+
+            if (order.State == OrderState.Ordered)
+            {
+                string actualDelivery = ((TextBox)OrderDetailsView.FindControl("ActualDeliveryBox")).Text;
+                OrderService.UpdateActualDeliveryDate(order.Id, parseDateTime(actualDelivery));
+            }
+            OrderService.ProceedToNextState(order.Id);
+            // display updated data
+            OrderDetailsView.DataBind();
+        }
+
+        private static DateTime parseDateTime(string dateAsString)
+        {
+            return DateTime.ParseExact(dateAsString, "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
         }
 
         protected void Cancel_Command(object sender, CommandEventArgs e)
         {
             // TODO cancel order
-            Server.Transfer("~/ReplenishmentOrder/List.aspx");
         }
     }
 }
