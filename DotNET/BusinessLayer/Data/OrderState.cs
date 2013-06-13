7e608f138aa4441854015c5e5eda535525da9d38
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-13 14:32:10 +0200

    Add business logic to state enum

--- DotNET/BusinessLayer/Data/OrderState.cs
+++ DotNET/BusinessLayer/Data/OrderState.cs
@@ -10,4 +10,33 @@ namespace Pharmacy.BusinessLayer.Data
     {
         Open, Posting, Ordered, Finished, Cancelled
     }
+
+    public static class OrderStateExtension
+    {
+        public static OrderState Next(this OrderState currentState)
+        {
+            switch (currentState)
+            {
+                case OrderState.Open:
+                    return OrderState.Posting;
+                case OrderState.Posting:
+                    return OrderState.Ordered;
+                case OrderState.Ordered:
+                    return OrderState.Finished;
+                default:
+                    // to have a reasonable default behaviour (instead of throwing an exception)
+                    return currentState;
+            }
+        }
+
+        public static Boolean Proceedable(this OrderState state)
+        {
+            return !(state == OrderState.Finished || state == OrderState.Cancelled);
+        }
+
+        public static Boolean Cancellable(this OrderState state)
+        {
+            return state == OrderState.Posting;
+        }
+    }
 }
