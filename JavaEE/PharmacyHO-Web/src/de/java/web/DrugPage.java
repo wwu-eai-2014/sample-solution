daf5e1555615a8596c553c9cdf1bd079068299d7
Jan Ernsting <jan.ernsting@gmail.com>
2014-06-22 22:53:31 +0200

    Add details page for drugs

---
+++ JavaEE/PharmacyHO-Web/src/de/java/web/DrugPage.java
@@ -0,0 +1,69 @@
+package de.java.web;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import de.java.domain.Drug;
+import de.java.ejb.DrugService;
+import de.java.web.util.Util;
+
+@ManagedBean
+@ViewScoped
+public class DrugPage implements Serializable {
+  private static final long serialVersionUID = -7325343421585931495L;
+
+  @EJB
+  private DrugService drugService;
+
+  private int pzn;
+  private Drug drug;
+
+  public int getPzn() {
+    return pzn;
+  }
+
+  public void setPzn(int pzn) {
+    this.pzn = pzn;
+    init();
+  }
+
+  private void init() {
+    drug = null;
+  }
+
+  public void ensureInitialized(){
+    try{
+      if(getDrug() != null)
+        // Success
+        return;
+    } catch(EJBException e) {
+      e.printStackTrace();
+    }
+    Util.redirectToRoot();
+  }
+
+  public String submitMasterDataChanges() {
+    drug = drugService.updateMasterData(drug.getPzn(), drug.getName(), drug.getDescription());
+    return toDrugPage();
+  }
+
+  private String toDrugPage() {
+    return "/drug/details.xhtml?faces-redirect=true&pzn=" + pzn;
+  }
+
+  public Drug getDrug() {
+    if (drug == null) {
+      drug = drugService.getDrug(pzn);
+    }
+    return drug;
+  }
+
+  public void setDrug(Drug drug) {
+    this.drug = drug;
+  }
+
+}
